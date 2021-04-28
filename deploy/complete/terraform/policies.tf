a7c55ad340e68145473849f12f5330c13ef100da
Pavol Gressa <pavol.gressa@oracle.com>
2021-04-28 18:43:27 +0200

    fix typo

--- deploy/complete/terraform/policies.tf
+++ deploy/complete/terraform/policies.tf
@@ -94,12 +94,12 @@ locals {
     "Allow group ${var.user_admin_group_for_vault_policy} to manage vaults in compartment id ${local.oke_compartment_ocid}",
     "Allow group ${var.user_admin_group_for_vault_policy} to manage keys in compartment id ${local.oke_compartment_ocid}",
     "Allow group ${var.user_admin_group_for_vault_policy} to use key-delegate in compartment id ${local.oke_compartment_ocid}"
-  ]
+  ]y
   allow_oke_use_monitoring = [
-    "Allow group ${var.user_admin_group_for_vault_policy} to use apm-domains in compartment id ${local.oke_compartment_ocid}",
-    "Allow group ${var.user_admin_group_for_vault_policy} to use metrics in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${local.oke_nodes_dg} to use apm-domains in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${local.oke_nodes_dg} to use metrics in compartment id ${local.oke_compartment_ocid}"
   ]
   allow_oke_to_manage_atp = [
-    "Allow group ${var.user_admin_group_for_vault_policy} to manage autonomous-database-family in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${local.oke_nodes_dg} to manage autonomous-database-family in compartment id ${local.oke_compartment_ocid}"
   ]
 }

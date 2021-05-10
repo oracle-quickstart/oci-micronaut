9af6824609c242964033c2d44ffd55d9cdca4391
Pavol Gressa <pavol.gressa@oracle.com>
2021-05-10 15:32:54 +0200

    feat: add config endpoint

---
+++ src/api/src/main/java/api/services/ConfigService.java
@@ -0,0 +1,33 @@
+package api.services;
+
+import api.services.annotation.MuService;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.session.Session;
+import io.micronaut.session.http.HttpSessionFilter;
+import io.reactivex.Single;
+
+import java.util.Map;
+
+@MuService
+@Secured(SecurityRule.IS_ANONYMOUS)
+public class ConfigService {
+
+    @Get("/config")
+    Single<Map<String, Object>> getConfig(HttpRequest<?> request) {
+        final Session session
+                = request.getAttribute(HttpSessionFilter.SESSION_ATTRIBUTE, Session.class)
+                .orElse(null);
+        String trackId = "";
+        if (session != null) {
+            trackId = session.getId();
+        }
+        return Single.just(Map.of(
+                "trackId", trackId,
+                "mockMode", false,
+                "staticAssetPrefix", ""
+        ));
+    }
+}

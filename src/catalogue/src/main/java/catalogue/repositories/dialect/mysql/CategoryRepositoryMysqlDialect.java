e0ed1b3744f9abce9d2b9e2e4cfd6521fbfa25b4
Pavol Gressa <pavol.gressa@oracle.com>
2021-06-08 17:31:29 +0200

    fixup! catalogue

--- src/catalogue/src/main/java/catalogue/repositories/dialect/mysql/CategoryRepositoryMysqlDialect.java
+++ src/catalogue/src/main/java/catalogue/repositories/dialect/mysql/CategoryRepositoryMysqlDialect.java
@@ -1,9 +1,15 @@
 package catalogue.repositories.dialect.mysql;
 
 import catalogue.repositories.CategoryRepository;
+import catalogue.repositories.dialect.oracle.CategoryRepositoryOracleDialect;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 
+@Requires(env = Environment.AMAZON_EC2)
+@Replaces(CategoryRepositoryOracleDialect.class)
 @JdbcRepository(dialect = Dialect.MYSQL)
 public interface CategoryRepositoryMysqlDialect extends CategoryRepository {
 }
--- src/catalogue/src/main/java/catalogue/repositories/dialect/mysql/ProductRepositoryMysqlDialect.java
+++ src/catalogue/src/main/java/catalogue/repositories/dialect/mysql/ProductRepositoryMysqlDialect.java
@@ -1,9 +1,15 @@
 package catalogue.repositories.dialect.mysql;
 
 import catalogue.repositories.ProductRepository;
+import catalogue.repositories.dialect.oracle.ProductRepositoryOracleDialect;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 
+@Requires(env = Environment.AMAZON_EC2)
+@Replaces(ProductRepositoryOracleDialect.class)
 @JdbcRepository(dialect = Dialect.MYSQL)
 public interface ProductRepositoryMysqlDialect extends ProductRepository {
 }

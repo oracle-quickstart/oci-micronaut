080ff2fb533fea6ee1b64336488cc4c1a6023c51
Pavol Gressa <1838702+pgressa@users.noreply.github.com>
2021-06-15 15:48:32 +0200

    api: native image supported (#82)

--- src/api/src/main/java/api/services/support/CartIdBinder.java
+++ src/api/src/main/java/api/services/support/CartIdBinder.java
@@ -6,6 +6,8 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.bind.binders.AnnotatedRequestArgumentBinder;
 import io.micronaut.session.Session;
 import io.micronaut.session.http.HttpSessionFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 import java.util.Optional;
@@ -13,7 +15,7 @@ import java.util.UUID;
 
 @Singleton
 public class CartIdBinder implements AnnotatedRequestArgumentBinder<CartId, UUID> {
-
+    private static final Logger LOG = LoggerFactory.getLogger(CartIdBinder.class);
     public static final String CART_ID = "cartId";
 
     @Override
@@ -28,11 +30,13 @@ public class CartIdBinder implements AnnotatedRequestArgumentBinder<CartId, UUID
             final Session session = attribute.get();
             final UUID uuid = session.get(CART_ID, UUID.class).orElseGet(() -> {
                 final UUID newUUID = UUID.randomUUID();
+                LOG.info("Generating new session ID " + newUUID);
                 session.put(CART_ID, newUUID);
                 return newUUID;
             });
             return () -> Optional.of(uuid);
         } else {
+            LOG.info("Session not found in the request");
             return BindingResult.UNSATISFIED;
         }
     }

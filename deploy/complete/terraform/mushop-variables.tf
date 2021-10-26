73133c44df7d0d1039d5fccd2f8ae3c1b8eb8a04
Pavol Gressa <pavol.gressa@oracle.com>
2021-10-26 11:02:51 +0200

    upgrade terraform

--- deploy/complete/terraform/mushop-variables.tf
+++ deploy/complete/terraform/mushop-variables.tf
@@ -4,9 +4,43 @@
 
 # MuShop
 ## Ingress/LoadBalancer
-variable "ingress_load_balancer_shape" {
-  default = "100Mbps"
+variable "ingress_nginx_enabled" {
+  default     = true
+  description = "Enable Ingress Nginx for Kubernetes Services (This option provision a Load Balancer)"
 }
+variable "ingress_load_balancer_shape" {
+  default     = "flexible" # Flexible, 10Mbps, 100Mbps, 400Mbps or 8000Mps
+  description = "Shape that will be included on the Ingress annotation for the OCI Load Balancer creation"
+}
+variable "ingress_load_balancer_shape_flex_min" {
+  default     = "10"
+  description = "Enter the minimum size of the flexible shape."
+}
+variable "ingress_load_balancer_shape_flex_max" {
+  default     = "100"
+  description = "Enter the maximum size of the flexible shape (Should be bigger than minimum size). The maximum service limit is set by your tenancy limits."
+}
+variable "ingress_hosts" {
+  default     = ""
+  description = "Enter a valid full qualified domain name (FQDN). You will need to map the domain name to the EXTERNAL-IP address on your DNS provider (DNS Registry type - A). If you have multiple domain names, include separated by comma. e.g.: mushop.example.com,catshop.com"
+}
+variable "cert_manager_enabled" {
+  default     = true
+  description = "Enable x509 Certificate Management"
+}
+variable "ingress_tls" {
+  default     = false
+  description = "If enabled, will generate SSL certificates to enable HTTPS for the ingress using the Certificate Issuer"
+}
+variable "ingress_cluster_issuer" {
+  default     = "letsencrypt-prod"
+  description = "Certificate issuer type. Currently supports the free Let's Encrypt and Self-Signed. Only *letsencrypt-prod* generates valid certificates"
+}
+variable "ingress_email_issuer" {
+  default     = "no-reply@mushop.ateam.cloud"
+  description = "You must replace this email address with your own. The certificate provider will use this to contact you about expiring certificates, and issues related to your account."
+}
+
 
 ## Features
 variable "mushop_mock_mode_all" {
@@ -41,14 +75,6 @@ variable "metrics_server_enabled" {
 variable "catalog_enabled" {
   default     = false
   description = "Enable Service Catalog to use with OCI Service Broker"
-}
-variable "ingress_nginx_enabled" {
-  default     = true
-  description = "Enable Ingress Nginx for Kubernetes Services (This option provision a Load Balancer)"
-}
-variable "cert_manager_enabled" {
-  default     = true
-  description = "Enable x509 Certificate Management"
 }
 
 ## Secrets
@@ -95,18 +121,6 @@ variable "autonomous_database_data_safe_status" {
   }
 }
 
-variable "apm_zipkin_enabled" {
-  default = true
-}
-
-variable "apm_zipkin_url" {
-  default = ""
-}
-
-variable "apm_zipkin_path" {
-  default = ""
-}
-
 variable "autonomous_database_db_version" {
   default = "19c"
 }
@@ -137,6 +151,18 @@ variable "autonomous_database_visibility" {
 }
 variable "autonomous_database_wallet_generate_type" {
   default = "SINGLE"
+}
+
+variable "apm_zipkin_enabled" {
+  default = true
+}
+
+variable "apm_zipkin_url" {
+  default = ""
+}
+
+variable "apm_zipkin_path" {
+  default = ""
 }
 
 variable "newsletter_service_name" {

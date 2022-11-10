7f914f7661b3b5ab97c567e928dc2dade0ed33ef
Milenko Supic <supic82@gmail.com>
2022-11-10 10:40:05 +0100

    Refactoring the orders service to use multi project build approach

---
+++ src/orders/aws/src/test/java/mushop/orders/entities/OrderEntityUnitTest.java
@@ -0,0 +1,7 @@
+package mushop.orders.entities;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+
+@MicronautTest
+public class OrderEntityUnitTest extends AbstractOrderEntityUnitTest {
+}

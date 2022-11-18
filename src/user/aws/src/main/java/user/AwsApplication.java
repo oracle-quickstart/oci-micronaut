a9ced510bf24de76860691f658df28d7964f8ef3
Milenko Supic <supic82@gmail.com>
2022-11-18 12:45:21 +0100

    Fixed usage of web identity credentials when native images run in AWS

--- src/user/aws/src/main/java/user/AwsApplication.java
+++ src/user/aws/src/main/java/user/AwsApplication.java
@@ -1,8 +1,14 @@
 package user;
 
 import io.micronaut.context.env.Environment;
+import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
+import software.amazon.awssdk.services.sts.internal.StsWebIdentityCredentialsProviderFactory;
 
+@TypeHint(
+        value = {StsWebIdentityCredentialsProviderFactory.class},
+        accessType = {TypeHint.AccessType.ALL_PUBLIC_CONSTRUCTORS}
+)
 public class AwsApplication {
     public static void main(String[] args) {
         Micronaut.build(args)

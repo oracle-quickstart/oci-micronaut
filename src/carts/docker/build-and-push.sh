1291c412c6763ebb162949845ec462068d40360d
Milenko Supic <supic82@gmail.com>
2022-11-21 15:50:28 +0100

    Fixed certificate issue when carts running as native image in AWS (#64)
    Fixed certificate issue when carts running as native image in AWS

--- src/carts/docker/build-and-push.sh
+++ src/carts/docker/build-and-push.sh
@@ -5,6 +5,7 @@ REPOSITORY=oraclelabs/micronaut-showcase/mushop
 
 OPENJDK_TAG=16-alpine
 GRAALVM_TAG=java11-21.1.0
+NATIVE_TAG=ol7-java11-22.2.0
 
 
 docker build -t ${REGISTRY}/${REPOSITORY}/base/openjdk:${OPENJDK_TAG} -f Dockerfile.base-openjdk .
@@ -12,3 +13,6 @@ docker push ${REGISTRY}/${REPOSITORY}/base/openjdk:${OPENJDK_TAG}
 
 docker build -t ${REGISTRY}/${REPOSITORY}/base/graalvm-ce:${GRAALVM_TAG} -f Dockerfile.base-graalvm .
 docker push ${REGISTRY}/${REPOSITORY}/base/graalvm-ce:${GRAALVM_TAG}
+
+docker build -t ${REGISTRY}/${REPOSITORY}/base/native:${NATIVE_TAG} -f Dockerfile.base-native .
+docker push ${REGISTRY}/${REPOSITORY}/base/native:${NATIVE_TAG}

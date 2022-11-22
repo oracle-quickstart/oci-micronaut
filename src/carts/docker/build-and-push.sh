43b87a6021dd876d609f0e3ead6df294e028cddc
Milenko Supic <supic82@gmail.com>
2022-11-22 13:47:01 +0100

    Fixed certificate issue when events service running as native image in AWS (#65)
    Fixed certificate issue when events service running as native image in AWS

--- src/carts/docker/build-and-push.sh
+++ src/carts/docker/build-and-push.sh
@@ -6,6 +6,7 @@ REPOSITORY=oraclelabs/micronaut-showcase/mushop
 OPENJDK_TAG=16-alpine
 GRAALVM_TAG=java11-21.1.0
 NATIVE_TAG=ol7-java11-22.2.0
+ALPINE_GLIBC_TAG=alpine-3.12
 
 
 docker build -t ${REGISTRY}/${REPOSITORY}/base/openjdk:${OPENJDK_TAG} -f Dockerfile.base-openjdk .
@@ -16,3 +17,6 @@ docker push ${REGISTRY}/${REPOSITORY}/base/graalvm-ce:${GRAALVM_TAG}
 
 docker build -t ${REGISTRY}/${REPOSITORY}/base/native:${NATIVE_TAG} -f Dockerfile.base-native .
 docker push ${REGISTRY}/${REPOSITORY}/base/native:${NATIVE_TAG}
+
+docker build -t ${REGISTRY}/${REPOSITORY}/base/alpine-glibc:${ALPINE_GLIBC_TAG} -f Dockerfile.base-alpine-glibc .
+docker push ${REGISTRY}/${REPOSITORY}/base/alpine-glibc:${ALPINE_GLIBC_TAG}

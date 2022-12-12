23d3acf68f66144e6018fe89323d0f71773770e1
Milenko Supic <supic82@gmail.com>
2022-12-12 14:48:02 +0100

    Creating micronaut assets service

---
+++ src/assets/aws/src/main/java/assets/AwsAssetUploadHandler.java
@@ -0,0 +1,36 @@
+package assets;
+
+import io.micronaut.objectstorage.aws.AwsS3Operations;
+import io.micronaut.objectstorage.request.UploadRequest;
+import jakarta.inject.Singleton;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+
+import java.util.Set;
+
+@Singleton
+public class AwsAssetUploadHandler extends AssetUploadHandler {
+
+    private final AwsS3Operations objectStorage;
+
+    public AwsAssetUploadHandler(AwsS3Operations objectStorage) {
+        this.objectStorage = objectStorage;
+    }
+
+    @Override
+    Set<String> listAssets() {
+        return objectStorage.listObjects();
+    }
+
+    @Override
+    void uploadAsset(UploadRequest uploadRequest) {
+        objectStorage.upload(uploadRequest, builder -> {
+            builder.acl(ObjectCannedACL.PUBLIC_READ);
+        });
+    }
+
+    @Override
+    void deleteAsset(String assetKey) {
+        objectStorage.delete(assetKey);
+    }
+
+}
---
+++ src/assets/oci/src/main/java/assets/OciAssetUploadHandler.java
@@ -0,0 +1,36 @@
+package assets;
+
+import io.micronaut.objectstorage.oraclecloud.OracleCloudStorageOperations;
+import io.micronaut.objectstorage.request.UploadRequest;
+import jakarta.inject.Singleton;
+
+import java.util.Map;
+import java.util.Set;
+
+@Singleton
+public class OciAssetUploadHandler extends AssetUploadHandler {
+
+    private final OracleCloudStorageOperations objectStorage;
+
+    public OciAssetUploadHandler(OracleCloudStorageOperations objectStorage) {
+        this.objectStorage = objectStorage;
+    }
+
+    @Override
+    Set<String> listAssets() {
+        return objectStorage.listObjects();
+    }
+
+    @Override
+    void uploadAsset(UploadRequest uploadRequest) {
+        objectStorage.upload(uploadRequest, builder -> {
+            builder.opcMeta(Map.of("project", "MuShop"));
+        });
+    }
+
+    @Override
+    void deleteAsset(String assetKey) {
+        objectStorage.delete(assetKey);
+    }
+
+}

1bab23ed90ee2506efced219cd5c1db1feaef1d9
Chaimaa Rouai <chaimaa.rouai@oracle.com>
2024-06-03 11:44:20 +0100

    fix tests faillure

--- src/events/app/src/main/java/events/model/EventRecord.java
+++ src/events/app/src/main/java/events/model/EventRecord.java
@@ -10,18 +10,17 @@ import java.util.Objects;
 /**
  * Event record.
  */
-// import java.time.Instant;
-// import java.util.Map;
-// import java.util.Objects;
+
 
 @Serdeable
 public record EventRecord(String source, String track, Instant time, Event event) {
 
+    @Creator
     public EventRecord(String source, String track, Event event) {
         this(source, track, Instant.now(), Objects.requireNonNull(event, "Event cannot be null"));
     }
 
-    @Creator
+
     public EventRecord(String source, String track, String type, Map<String, String> detail) {
         this(source, track, Instant.now(), new Event(type, detail));
     }
--- src/events/app/src/main/java/events/model/EventsReceived.java
+++ src/events/app/src/main/java/events/model/EventsReceived.java
@@ -8,27 +8,4 @@ import io.micronaut.serde.annotation.Serdeable;
 @Serdeable
 public record EventsReceived(boolean success ,int events){
 }
-// public class EventsReceived {
 
-//     private final boolean success;
-//     private final int events;
-
-//     public EventsReceived(boolean success, int events) {
-//         this.success = success;
-//         this.events = events;
-//     }
-
-//     /**
-//      * True if the events were successfully received
-//      */
-//     public boolean isSuccess() {
-//         return success;
-//     }
-
-//     /**
-//      * The number of successfully received events
-//      */
-//     public int getEvents() {
-//         return events;
-//     }
-// }
